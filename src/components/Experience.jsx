8a6fcb60af375d657a8c2d1d68a2fd98766c684d
rosie-bradstock <rgb1909@gmail.com>
2023-09-06 00:25:52 +0100

    pushing again to make changes visible

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
             </div>
             <div className='w-full grid grid-cols-5 sm:grid-cols-9 gap-4 py-8'>
               <div className="col-span-2">
-                <img src={template_image} alt="Project 2" width="300" height="300" />
+                <img src={template_image} alt="Work Experience Picture" width="300" height="300" />
               </div>
               <div className="col-span-2">
                 <p className="text-indigo-200 sm:text-sm md:text-md lg:text-xl">work experience text</p>
@@ -19,16 +19,16 @@ const Experience = () => {
               <div></div>
               
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
 
               <div className="col-span-5 sm:col-span-9"></div>
@@ -37,22 +37,22 @@ const Experience = () => {
                 <p className="text-indigo-200 sm:text-sm md:text-md lg:text-xl">work experience text</p>
               </div>
               <div className="col-span-2">
-                <img src={template_image} alt="Project 2" width="300" height="300" />
+                <img src={template_image} alt="Work Experience Picture" width="300" height="300" />
               </div>
 
               <div></div>
 
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
               <div>
-                <img src={template_image} alt="Project 2" width="200" height="200" />
+                <img src={template_image} alt="Skill Picture" width="200" height="200" />
               </div>
             </div>
         </div>
